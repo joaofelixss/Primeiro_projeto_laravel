79416bf8da4d8f111e02a71b0bb61db5d505deea
João Félix <felixjoao651@gmail.com>
2023-08-02 15:13:35 -0400

    Aprendendo melhor sobre o blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "João";
+    $arr = [1,2,3,4,5];
+
+    return view('welcome', ['nome' => $nome, 'arr' => $arr]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
